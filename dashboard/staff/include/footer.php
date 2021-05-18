83762d392a1ea97041e7099d7be7c5b8ce0da1b1
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-05-18 18:58:45 +0100

    dashboard

---
+++ dashboard/staff/include/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; <?php echo date("Y"); ?> <a href="https://doteightplus.ccom">School Management System Demo</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Developed by</b> <a target="_blank" href="https:/doteightplus.com">DotEightPlus</a>
+    </div>
+  </footer>
